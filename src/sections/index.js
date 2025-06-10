ab41dae1e984c7872764dc1cdddf3d0a12622447
smh653 <mukherjee.syon1816@gmail.com>
2025-06-10 17:45:16 +0530

    Initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import SuperQuality from './SuperQuality';
+import SpecialOffer from './SpecialOffer';
+import Services from './Services';
+import PopularProducts from './PopularProducts';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+export {
+    Hero,
+    SuperQuality,
+    SpecialOffer,
+    Services,
+    PopularProducts,
+    CustomerReviews,
+    Subscribe,
+    Footer  
+}
